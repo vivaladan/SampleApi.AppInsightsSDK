ac18da76e54aec4b6cda4f39db16c045818ffd17
Daniel Revell <daniel.revell@navicogroup.com>
2024-01-31 14:47:14 +0000

    message

--- SampleApi.AppInsightsSDK/Program.cs
+++ SampleApi.AppInsightsSDK/Program.cs
@@ -1,6 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddApplicationInsightsTelemetry();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () =>
+{
+    app.Logger.LogWarning("Get invoked. Using App Insights SDK");
+    return "Hello World! Using App Insights SDK";
+});
 
 app.Run();
